72378f01b3f9715ef249fa38ba53e0c74c93184d
ROL <romanlopatin@gmail.com>
2022-06-03 18:26:39 +0300

    L-2(course 2)

--- launcher.py
+++ launcher.py
@@ -11,10 +11,15 @@ while True:
     if ACTION == 'q':
         break
     elif ACTION == 's':
-        PROCESS.append(subprocess.Popen('python server.py',
+        try:
+            client_num = int(input('Введите число клиентов для запуска: '))
+        except ValueError:
+            client_num = 2
+
+        PROCESS.append(subprocess.Popen('python server.py -p 7777 -a 127.0.0.1',
                                         creationflags=subprocess.CREATE_NEW_CONSOLE))
 
-        for i in range(1):
+        for i in range(client_num):
             PROCESS.append(subprocess.Popen(f'python client.py 127.0.0.1 7777 -n test_{i + 1} ',
                                             creationflags=subprocess.CREATE_NEW_CONSOLE))
 
