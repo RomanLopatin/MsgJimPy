300c22723473bb17ee6a0788bdda1f5a39f62c53
ROL <romanlopatin@gmail.com>
2022-06-29 19:18:38 +0300

    Сделана документация sphinx-doc

--- common/errors.py
+++ common/errors.py
@@ -2,7 +2,7 @@
 
 
 class ReqFieldMissingError(Exception):
-    """Ошибка - отсутствует обязательное поле в принятом словаре"""
+    """Исключение - отсутствует обязательное поле в принятом словаре"""
 
     def __init__(self, missing_field):
         self.missing_field = missing_field
@@ -13,12 +13,14 @@ class ReqFieldMissingError(Exception):
 
 class IncorrectDataRecivedError(Exception):
     """Исключение  - некорректные данные получены от сокета """
+
     def __str__(self):
         return 'Принято некорректное сообщение от удалённого компьютера.'
 
 
 class ServerError(Exception):
     """Исключение - ошибка сервера"""
+
     def __init__(self, text):
         self.text = text
 
